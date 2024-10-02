8091ad9678be0cc9d6013d94ead922e98e9e81b9
Eric Horodyski <horodyski@ionic.io>
2024-10-02 16:32:33 -0400

    feat: generate app icon script

--- scripts/generate-icon.ts
+++ scripts/generate-icon.ts
@@ -10,7 +10,6 @@ import {
 // Path inputs
 const projectRoot = "../";
 const pathToLogo = "assets/logo.png";
-const pathToLogoDark = "assets/logo-dark.png";
 
 // Project configuration
 const capacitorProjectConfig = {
@@ -22,23 +21,17 @@ const project = new MobileProject(projectRoot, capacitorProjectConfig);
 // Asset generation options
 const opts = {
   iconBackgroundColor: "#cccccc",
-  iconBackgroundColorDark: "#282828",
 };
 
-async function generateIcons() {
-  const assets = [
-    //new InputAsset(pathToLogo, AssetKind.Logo, Platform.Any),
-    new InputAsset(pathToLogoDark, AssetKind.LogoDark, Platform.Any),
-  ];
+async function generateAppIcon() {
+  const asset = new InputAsset(pathToLogo, AssetKind.Logo, Platform.Any);
   const generators = [new IosAssetGenerator(opts), new AndroidAssetGenerator(opts)];
 
   try {
-    for (const asset of assets) {
-      await Promise.all(generators.map((g) => g.generate(asset, project)));
-    }
+    await Promise.all(generators.map((g) => g.generate(asset, project)));
     console.log("Icons generated");
   } catch (error) {
     console.error("Icon generation failed.");
   }
 }
-await generateIcons();
+await generateAppIcon();
